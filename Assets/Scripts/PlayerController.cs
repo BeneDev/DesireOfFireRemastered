d1aa6e3c8b1e17b1ab9dd7d5713ce002bdcd5de2
MirroredFate <Karl_daniel@hotmail.de>
2018-01-30 11:43:12 +0100

    Updated playercontroller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,14 +6,19 @@ public class PlayerController : MonoBehaviour {
 
     Vector3 fwd;
 
+    public GameObject projectilePrefab;
+    public GameObject player;
+
 	// Use this for initialization
 	void Start () {
-		
+	    
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKeyDown(KeyCode.E){
+            GameObject projectile = Instantiate(projectilePrefab, player.transform.position, Quaternion.identity);
+        }
 	}
 
     private void FixedUpdate()
