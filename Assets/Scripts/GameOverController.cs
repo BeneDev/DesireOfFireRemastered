cf2b1608f4a8383358ff240e4cb3f70a6d2b7519
Bene_Dev <seriousbenedikt@web.de>
2018-01-31 15:22:32 +0100

    GameOverScreen

--- Assets/Scripts/GameOverController.cs
+++ Assets/Scripts/GameOverController.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MenuController : MonoBehaviour {
+public class GameOverController : MonoBehaviour {
 
-    public void PlayGame()
+    public void TryAgain()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(1);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 
     public void ExitGame()
