7d89003f5a3788c8e2ec13d9bd5cdf3fea0920f0
Bene_Dev <seriousbenedikt@web.de>
2018-02-06 08:37:57 +0100

    Heartbeat sound if player health low

---
+++ Assets/Scripts/LowLifeMusicController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LowLifeMusicController : MonoBehaviour {
+
+    GameObject player;
+    AudioSource aS;
+
+	// Use this for initialization
+	void Start () {
+        aS = GetComponent<AudioSource>();
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(player.GetComponent<PlayerController>().health <= 30 && !aS.isPlaying)
+        {
+            aS.Play();
+        }
+        else if(player.GetComponent<PlayerController>().health > 30 && aS.isPlaying)
+        {
+            aS.Stop();
+        }
+	}
+}
